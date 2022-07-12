1c6fabcdb8a032e3c80d055143a7e427592aba3a
Kame15-2021 <21cm0108@jec.ac.jp>
2022-07-12 17:26:39 +0900

    Initial Commit

---
+++ CombineClock08/CombineClock08App.swift
@@ -0,0 +1,17 @@
+//
+//  CombineClock08App.swift
+//  CombineClock08
+//
+//  Created by Ryo on 2022/07/12.
+//
+
+import SwiftUI
+
+@main
+struct CombineClock08App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
