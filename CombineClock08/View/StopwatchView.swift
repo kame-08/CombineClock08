0107bb7c30b705e814a6cae381ef811e77f08745
Kame15-2021 <21cm0108@jec.ac.jp>
2022-07-16 12:00:45 +0900

    Fix:画面サイズが小さい端末でレイアウトが崩れる(タイムが2行になる)の修正 画面を縦画面のみ許可に変更

--- CombineClock08/View/StopwatchView.swift
+++ CombineClock08/View/StopwatchView.swift
@@ -18,17 +18,22 @@ struct StopwatchView: View {
             VStack {
                 ZStack{
                     Circle()
-                        .padding(.horizontal)
+                        .frame(width: UIScreen.main.bounds.width, height:  UIScreen.main.bounds.width)
                     
                     Text("\(viewModel.countFormat)")
                         .font(Font(UIFont.monospacedDigitSystemFont(ofSize: 90, weight: .thin)))
+                    //小さい画面サイズの対応
+                    //1列になるように
+                        .lineLimit(1)
+                    //自動で文字が縮小されるように
+                        .minimumScaleFactor(0.1)
                     //.font(.footnote)
                         .foregroundColor(Color.white)
                     
                 }
                 
                 ButtonView(viewModel: viewModel)
-                    .offset(x: 0, y: -60)
+                    .offset(x: 0, y: -60.0)
                     .padding(.bottom, -60.0)
                 
                 Spacer()
